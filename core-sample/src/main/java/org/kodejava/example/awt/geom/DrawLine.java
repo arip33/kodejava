62fb7259301c01ce79d9180fc58da2ddc6471a29
wsaryada <wsaryada@yahoo.com>
2012-05-21 17:34:15 +0800

    Update awt.geom examples

--- core-sample/src/main/java/org/kodejava/example/awt/geom/DrawLine.java
+++ core-sample/src/main/java/org/kodejava/example/awt/geom/DrawLine.java
@@ -3,12 +3,34 @@ package org.kodejava.example.awt.geom;
 import javax.swing.*;
 import java.awt.*;
 
-public class DrawLine {
+public class DrawLine extends JApplet {
+    @Override
+    public void paint(Graphics g) {
+        //
+        // Draw a simple line using the Graphics drawLine() method.
+        //
+        Graphics2D g2 = (Graphics2D) g;
+        g2.setStroke(new BasicStroke(2f));
+        g2.setColor(Color.RED);
+        g2.drawLine(50, 150, 250, 350);
+        g2.setColor(Color.GREEN);
+        g2.drawLine(250, 350, 350, 250);
+        g2.setColor(Color.BLUE);
+        g2.drawLine(350, 250, 150, 50);
+        g2.setColor(Color.YELLOW);
+        g2.drawLine(150, 50, 50, 150);
+        g2.setColor(Color.BLACK);
+        g2.drawLine(0, 0, 400, 400);
+    }
+
     public static void main(String[] args) {
         JFrame frame = new JFrame("Draw Line");
-        frame.setSize(new Dimension(400, 400));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JApplet applet = new DrawLine();
+        frame.getContentPane().add(applet);
         frame.pack();
+        frame.setSize(new Dimension(420, 440));
         frame.setVisible(true);
     }
 }
